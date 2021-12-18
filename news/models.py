02aa4223c0a00f946fd839007224376eda7596a7
Invisible <vitaliysomoniuk@gmail.com>
2021-12-18 19:27:46 +0200

    Add application news

---
+++ news/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Articles(models.Model):
+    title = models.CharField('Name', max_length=50, default='Article')
+    anons = models.CharField('Anons', max_length=200)
+    full_text = models.TextField('Article')
+    date = models.DateTimeField('Date of publication')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+
