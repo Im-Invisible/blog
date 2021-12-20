2a20409b8b5dc5b8e77e860f87672932c8c9a3d5
Invisible <vitaliysomoniuk@gmail.com>
2021-12-20 18:40:39 +0200

    Edited styles

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        'title': 'Invsiible blog',        
+        'title': 'Invisible blog',        
     }
     return render(request, 'main/index.html', data)
 
