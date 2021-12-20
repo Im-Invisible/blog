0b2d7c70f75396dd8448709ace98c22ba65f9f4d
Invisible <vitaliysomoniuk@gmail.com>
2021-12-20 17:45:46 +0200

    Added functionality dynamically variable pages

--- news/views.py
+++ news/views.py
@@ -2,6 +2,8 @@ from django import forms
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
+
 
 def news(request):
     news = Articles.objects.order_by('-date')
@@ -25,3 +27,9 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', data)
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
